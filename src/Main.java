ef4ef0cab9adf9422912f30241e00e2089b969ff
olababiuch <ola.babiuch7@gmail.com>
2025-06-15 20:02:05 -0400

    Task 2 completed

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("What's the yearly maintenance cost?");
+        int costSpring = 150;
+        int costSummer = 120;
+        int costFall = 180;
+        int costWinter = 240;
+        int costYearly;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        costYearly = costSpring + costSummer + costFall + costWinter;
+        System.out.println("The yearly maintenance cost is $" + costYearly + " if the cost in spring is $" +  costSpring + ", cost in summer is $" +  costSummer + ", cost in fall is $" + costFall + ", and cost in winter is $" + costWinter);
+
     }
 }
